f2ae2033a31a00abc0332464aa2b58b81551e8a9
endprasad <e.n.d.prasad@gmail.com>
2025-11-02 11:57:10 +0530

    Basic working version of the app

--- background.js
+++ background.js
@@ -15,22 +15,23 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 })
 
 async function  checkLinks(links) {
-    const results = []
+    const results = {skipped: [], valid: [], broken: [], redirected: []}
     for(let link of links) {
         if(link.type === 'skipped'){
-            results.push({url: link.url, status: 'skipped'})
+            results.skipped.push({url: link.url, status: 'skipped'})
             continue
         }
 
         try {
             const res = await fetch(link.url, {method: 'GET',  redirect: 'follow'})
             if(res.ok) {
-                results.push( {url: link.url, status: res.redirected ? 'redirected': 'valid'})
+                const key = res.redirected ? 'redirected': 'valid'
+                results[key].push( {url: link.url, status: key})
             }else {
-                results.push({url: link.url, status: 'broken', code: res.status})
+                results.broken.push({url: link.url, status: 'broken', code: res.status})
             }
         } catch (error) {
-            results.push({url: link.url, status: 'warning', message: error.message})
+            results.warning.push({url: link.url, status: 'warning', message: error.message})
         }
     }
     return results
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,9 @@
 const scanBtn = document.getElementById('scan')
+const totalLinksCount = document.getElementById('total')
+const validLinksCount = document.getElementById('valid')
+const brokenLinksCount = document.getElementById('broken')
+const redirectedLinksCount = document.getElementById('redirected')
+const skippedLinksCount = document.getElementById('skipped')
 
 
 
@@ -8,10 +13,21 @@ scanBtn.addEventListener('click', async ()=> {
     // Get current tab
     const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
 
-
+    // Collect all the links
     const {links} = await chrome.tabs.sendMessage(tab.id, {action: "collect_links"})
     console.log("Collected Links:", links)
 
-    const data = await chrome.tabs.sendMessage(tab.id, {action: 'check_links', links})
-    console.log(data)
+
+    // Display count in the UI
+    totalLinksCount.innerText = links.length
+    
+
+    // Check all link status
+    chrome.runtime.sendMessage({ action: "check_links", links }, response => {
+        console.log("Link check result:", response);
+        validLinksCount.innerText = response.results.valid.length
+        brokenLinksCount.innerText = response.results.broken.length
+        skippedLinksCount.innerText = response.results.skipped.length
+        redirectedLinksCount.innerText = response.results.redirected.length
+    });
 })
