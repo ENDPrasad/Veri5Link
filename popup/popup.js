3d6eee75ced5cae2dac03e46ce690b9997c3ab12
Sarath-sk <sarathsk758@gmail.com>
2025-11-03 23:26:10 +0530

    scan button functionality after clicking once

--- popup/popup.js
+++ popup/popup.js
@@ -10,13 +10,16 @@ const skippedLinksCount = document.getElementById('skipped')
 console.log("hello")
 scanBtn.addEventListener('click', async ()=> {
     console.log("Inside popUp JS")
+    scanBtn.setAttribute("disabled", true)
+    scanBtn.innerText = "Scanning..."
+    scanBtn.style.backgroundColor = "lightgrey"
+    scanBtn.style.color = "black"
     // Get current tab
     const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
 
     // Collect all the links
     const {links} = await chrome.tabs.sendMessage(tab.id, {action: "collect_links"})
     console.log("Collected Links:", links)
-
 
     // Display count in the UI
     totalLinksCount.innerText = links.length
@@ -29,5 +32,11 @@ scanBtn.addEventListener('click', async ()=> {
         brokenLinksCount.innerText = response.results.broken.length
         skippedLinksCount.innerText = response.results.skipped.length
         redirectedLinksCount.innerText = response.results.redirected.length
+        scanBtn.setAttribute("disabled", false)
+        scanBtn.innerText = "Scan"
+        scanBtn.style.backgroundColor = "rgb(64, 82, 181)"
+        scanBtn.style.color = "white"
     });
 })
+
+});
